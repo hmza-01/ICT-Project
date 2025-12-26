024207f51a56a8e75ed175e127138d1f88fa58ab
hashirirfan <mhashirirfan@gmail.com>
2025-12-26 11:11:15 +0500

    new photo

--- productsData.js
+++ productsData.js
@@ -4,7 +4,7 @@ export let products=[
   id:"1766490831329",
   quantity:1,
   name:"Pure Cane Jaggery",
-  url:"./AlSadiq/gurr.jpg",
+  url:"./AlSadiq/gurr2.png",
   description:"Rich, caramel notes with natural minerals. Unrefined and traditionally prepared."
 },
 {
@@ -12,7 +12,7 @@ export let products=[
   id:"1766490831330",
   quantity:1,
   name:"Brown-Sugar",
-  url:"./AlSadiq/gurr.jpg",
+  url:"./AlSadiq/gurr2.png",
   description:"Rich, caramel notes with natural minerals. Unrefined and traditionally prepared."
 },
 {
@@ -20,7 +20,7 @@ export let products=[
   id:"1766490831331",
   quantity:1,
   name:"Qand Crystals",
-  url:"./AlSadiq/gurr.jpg",
+  url:"./AlSadiq/gurr2.png",
   description:"Rich, caramel notes with natural minerals. Unrefined and traditionally prepared."
 },
 {
@@ -28,7 +28,7 @@ export let products=[
   id:"1766490831332",
   quantity:1,
   name:"Delicacies",
-  url:"./AlSadiq/gurr.jpg",
+  url:"./AlSadiq/gurr2.png",
   description:"Rich, caramel notes with natural minerals. Unrefined and traditionally prepared."
 },
 ]
